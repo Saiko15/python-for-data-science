eeffb8dcf58d122096921242b2d1f3a80fd5296a
Saiko15 <ahmedsaiko42@gmail.com>
2022-05-27 17:08:01 +0200

    Printing Homework '2'

--- Section (3) - Printing/04 Printing Homework/06_question.py
+++ Section (3) - Printing/04 Printing Homework/06_question.py
@@ -1,8 +1,8 @@
 
 
-print(Hi)
+print("Hi")
 print()
-print"I am mostafa")
+print("I am mostafa")
 print(3 * 6)
-print"Wonderful " day)
-print("Wonderful " day")
+print("Wonderful \" day")
+print("Wonderful \" day")

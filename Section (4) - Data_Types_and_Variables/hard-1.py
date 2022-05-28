2914a586a65e28357a06572909fa676080c48d32
Saiko15 <ahmedsaiko42@gmail.com>
2022-05-28 17:28:37 +0200

    answering Data Types and Variables hard homework

---
+++ Section (4) - Data_Types_and_Variables/hard-1.py
@@ -0,0 +1,12 @@
+num1, num2, num3 = map(int, input().split())
+
+num1, num2, num3= num2, num3, num1
+
+"""
+num4 = num1
+num1= num2
+num2=num3
+num3=num4
+"""
+
+print(num1, num2, num3)
---
+++ Section (4) - Data_Types_and_Variables/hard-2.py
@@ -0,0 +1,5 @@
+A, B = map(int, input().split())
+C= int((B*(-1) + 1) / 2)
+D=1-C
+
+print((A*A*D) + ((2*A + 1)*C ))

d5c7a24cdc8be5b3a52d972daaee586698f7b317
Saiko15 <ahmedsaiko@gmail.com>
2022-07-05 11:07:28 +0200

    adding dict and sets files

---
+++ Section(18) - Dicts and Sets/medium-1.py
@@ -0,0 +1,15 @@
+dct = dict(zip('abcdefghijklmnopqrstuvwxyz0123456789', 'YZIMNESTODUAPWXHQFBRJKCGVL!@#$%^&*()'))
+
+line = 'acMNmn39'
+line2 = 'vwXYZ0123'
+
+for char in line:
+    
+    print(dct.setdefault(char, char), end='')
+
+print()
+
+for char in line2:
+    print(dct.setdefault(char, char), end='')
+
+print(dct)
